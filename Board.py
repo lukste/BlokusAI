4c8dcc917bd4cdc2b4195324428a85e23937c02f
U166258 <lukasz.stefanski@nbp.pl>
2022-08-01 13:44:02 +0200

    Commit

--- Board.py
+++ Board.py
@@ -18,16 +18,29 @@ class Board:
         #     for j in range(self.n):
         #         print(self.status[i][j], end=',')
 
-    def place(self, block):
+    def checkPlace(self, block):
+        x,y  = block.position
+        col  = colors[block.colour]
         for row_id, row_val in enumerate(block.corners):
             for col_id, col_val in enumerate(row_val):
                 if col_val == True:
-                    self.status[col_id+block.position[0]][row_id + block.position[1]] = colors[block.colour]
-        print()
-        for i in range(self.n):
-            print()
-            for j in range(self.n):
-                print(self.status[j][i], end=',')
+                    if(self.status[col_id+x][row_id + y] != 0):
+                        return False
+                    if(self.status[col_id+x+1][row_id + y] == col or
+                       self.status[col_id+x-1][row_id + y] == col or
+                       self.status[col_id+x][row_id + y+1] == col or
+                       self.status[col_id+x][row_id + y-1] == col):
+                        return False
+        return True
+
+    def place(self, block):
+        if(self.checkPlace(block)):
+            for row_id, row_val in enumerate(block.corners):
+                for col_id, col_val in enumerate(row_val):
+                    if col_val == True:
+                        self.status[col_id+block.position[0]][row_id + block.position[1]] = colors[block.colour]
+        else:
+            print("Err")
 
     def draw(self, screen):
         for i in range(self.n):
